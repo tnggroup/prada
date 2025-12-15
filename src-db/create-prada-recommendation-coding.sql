535637c4f67d1c17b733db28ee3934f4c32615e3
Johan Källberg Zvrskovec <johan.akz@gmail.com>
2025-12-15 15:22:16 +0000

    condensed prada recommendation template

--- src-db/create-prada-recommendation-coding.sql
+++ src-db/create-prada-recommendation-coding.sql
@@ -11,3 +11,17 @@ ORDER BY drug_name, gene_name, pgx.diplotype, rn;
 
 SELECT * FROM t_recommendations_overview WHERE rn = 1
 ORDER BY drug_name, gene_name, diplotype, recommendation;
+
+--recommendations overview - starting template, condensed
+DROP TABLE IF EXISTS t_recommendations_overview2;
+CREATE TEMP TABLE IF NOT EXISTS t_recommendations_overview2 AS
+SELECT ROW_NUMBER() OVER (PARTITION BY pgx.drug_name, pgx.recommendation ORDER BY pgx.prada_cpiclevel_num DESC, pgx.prada_ehrpriority_num DESC, pgx.gene_name, pgx.diplotype NULLS LAST) AS rn, pgx.drug_name, pgx.drug_class, pgx.cpiclevel, pgx.prada_cpiclevel_num, pgx.recommendation, pgx.guidelineid, pgx.implications, pgx.drugrecommendation, pgx.classification, pgx.population, pgx.comments
+--pgx.drug_name, pgx.drug_class, pgx.gene_name, pgx.diplotype, pgx.description, pgx.result, pgx.ehrpriority, pgx.consultationtext, pgx.implications, pgx.drugrecommendations, pgx.phenotypes, pgx.classification, pgx.population, pgx.comments 
+FROM prada.harmonised_combined_pgx pgx 
+--INNER JOIN t_gene_diplotype_input g ON pgx.gene_name = g.gene AND pgx.diplotype = g.diplotype
+--INNER JOIN t_drug_input d ON pgx.drug_name = d.drugname
+WHERE pgx.classification != 'Optional'
+ORDER BY drug_name, gene_name, pgx.diplotype, rn;
+
+SELECT * FROM t_recommendations_overview2 WHERE rn = 1
+ORDER BY drug_name, recommendation;
