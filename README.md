43ed1e7568f2133389f0ff2d488a59db0604c333
Johan Källberg Zvrskovec <johan.akz@gmail.com>
2026-08-03 16:50:18 +0100

    cli interface

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-# prada
-An R package or scripts for the PRADA project
+# PGX Rex
+An R package or scripts to work with pharmacogenomics
 
 ## Project Structure
-prada/  
+pgxrex/  
 ├── data/       # Raw and/or processed datasets  
 ├── scripts/    # R and Python scripts for data analysis and processing  
-└── README.md   # Project description and structure 
-
-## Requirements
-- python 3.11.1
-- sqlalchemy 2.0.40  
-
-
-
-## Change-Log 
-### 13-06-2025
-#### Added
-- Final bed file under data/
-- modified script to generate from database under scripts/
-### 21-05-2025
-#### Added
-- SQL database comparison script for identifying data differences.
-- Python routine sketch for bed file generation
+└── README.md   # Project description and structure
---
+++ src-r/pgxrex-cli.R
@@ -0,0 +1,69 @@
+#PGX Rex CLI
+
+library(optparse)
+library(pgxrex)
+library(data.table)
+
+
+#command line options
+optionParser <- OptionParser()
+pgxrexObj<-PgxrexClass()
+
+optionParser <- add_option(
+  object = optionParser,
+  opt_str = c("-c", "--command"),
+  type = "character",
+  default = "automatic_routine",
+  help = "Command to run."
+)
+
+optionParser <- add_option(
+  object = optionParser,
+  opt_str = c("-s", "--settingsFilePath"),
+  type = "character",
+  default = "settings.conf.txt",
+  help = "Path to settings-file."
+)
+
+optionParser <- add_option(
+  object = optionParser,
+  opt_str = c("-p", "--protectedFilePath"),
+  type = "character",
+  default = "protected.conf.txt",
+  help = "Path to protected settings-file (passwords etc.)"
+)
+
+parsedCLOptions <- parse_args(optionParser)
+parsedCFOptions <- list(
+  `pgxrexId`="mySite",
+  `pgxrexDbHost`="localhost",
+  `pgxrexDbUsername`="tng_prada_system",
+  `pgxrexDbName`="prada_local",
+  `pgxrexDbPort`=65432
+  ) #defaults
+
+
+if(file.exists(parsedCLOptions$settingsFilePath)){
+  parsedCFOptions <- pgxrex::readMetadata(filePath = parsedCLOptions$settingsFilePath)
+} else {
+  if(!file.exists("settings.conf.txt")){
+    dfOpts<-data.table(names=names(parsedCFOptions), values=parsedCFOptions)
+    fwrite(dfOpts,"settings.conf.txt",sep = "=",row.names = FALSE,col.names = FALSE)
+  }
+}
+
+
+parsedPCFOptions <- list(
+  `pgxrexDbPassword`="XXX_replace_with_password_XXX"
+) #defaults
+
+
+if(file.exists(parsedCLOptions$protectedFilePath)){
+  parsedPCFOptions <- pgxrex::readMetadata(filePath = parsedCLOptions$protectedFilePath)
+} else {
+  if(!file.exists("protected.conf.txt")){
+    dfOpts<-data.table(names=names(parsedPCFOptions), values=parsedPCFOptions)
+    fwrite(dfOpts,"proetected.conf.txt",sep = "=",row.names = FALSE,col.names = FALSE)
+  }
+}
+
